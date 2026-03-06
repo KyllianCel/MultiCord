08680fed72d61bf383d06b0b5df7818b9fc6a58d
KyllianCel <kylliancelisse.pro@gmail.com>
2026-03-06 22:03:43 +0100

    modification dans le fichier ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,10 @@
-import { Events } from 'discord.js'
+import { Events, Client } from 'discord.js'
 import Event from '../templates/Event.js'
 
 export default new Event({
     name: Events.ClientReady,
     once: true,
-    execute(): void {
-        // Runs when the bot logs in
+    execute(client: Client): void {
         console.log(`Logged in as ${client.user?.tag as string}!`)
     }
 })
